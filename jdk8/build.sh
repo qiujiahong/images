e6c47aa35eae600e49caff8ee91d8f03d384c657
qiujiahong <qiujiahongde@163.com>
2019-07-03 22:53:56 +0800

    初始化镜像

---
+++ jdk8/build.sh
@@ -0,0 +1,4 @@
+docker build -t qiujiahong/jdk8:v1.0 .
+
+
+# docker push qiujiahong/jdk8:v1.0 
---
+++ jdk8/run.sh
@@ -0,0 +1,4 @@
+# docker run  -d -p 80:80 -e name='nick' qiujiahong/jdk8:v1.0
+
+
+docker run  -itd  qiujiahong/jdk8:v1.0
